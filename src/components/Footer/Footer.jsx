c4761cb1a13ea03904f0e7e5b636dbf7ed3c5999
Morgane <morgane.meganck@gmail.com>
2022-05-05 15:45:44 +0200

    footer base

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+  return (
+    <footer className="container text-center pb-5 footer">
+      <div>
+        2022 <span>&copy;</span> Morgane Meganck
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
