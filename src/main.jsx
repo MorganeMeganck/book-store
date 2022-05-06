16df8748cc6959ada754011331b06ef8960e4174
Morgane <morgane.meganck@gmail.com>
2022-05-06 15:35:15 +0200

    add redux persist

--- src/main.jsx
+++ src/main.jsx
@@ -8,18 +8,26 @@ import "./index.css";
 import { Provider } from "react-redux";
 import store from "./store/store";
 import NotFound from "./pages/errors/NotFound";
+import RegisterPage from "./pages/register/register";
+import { PersistGate } from "redux-persist/integration/react";
+import { persistStore } from "redux-persist";
+
+let persistor = persistStore(store);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<App />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/add" element={<AddBook />} />
-          <Route path="/*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<App />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/register" element={<RegisterPage />} />
+            <Route path="/add" element={<AddBook />} />
+            <Route path="/*" element={<NotFound />} />
+          </Routes>
+        </BrowserRouter>
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
--- src/store/store.js
+++ src/store/store.js
@@ -1,14 +1,39 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { createLogger } from "redux-logger";
 import userReducer from "./reducers/user-reducer";
+import storage from "redux-persist/lib/storage";
+import { combineReducers } from "redux";
+import {
+  FLUSH,
+  PAUSE,
+  PERSIST,
+  persistReducer,
+  PURGE,
+  REGISTER,
+  REHYDRATE,
+} from "redux-persist";
+const reducers = combineReducers({
+  user: userReducer,
+});
 
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, reducers);
 const logger = createLogger();
 
 const store = configureStore({
-  reducer: {
-    user: userReducer,
-  },
-  middleware: (getDefaultMiddleware) => [...getDefaultMiddleware(), logger],
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) => [
+    ...getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+    logger,
+  ],
   devTools: process.env.NODE_ENV === "development",
 });
 
